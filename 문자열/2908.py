c8981cfb73ff8f4c95212c5b450dfc4ce69c2c19
purmpu <84269579+purmpu@users.noreply.github.com>
2022-01-29 17:55:26 +0900

    Update 2908.py

--- 문자열/2908.py
+++ 문자열/2908.py
@@ -1,8 +1,13 @@
-num1, num2 = input().split()
-num1 = int(num1[::-1])  
-num2 = int(num2[::-1])
+A,B = input().split()
+A = int(A[::-1])
+B = int(B[::-1])
 
-if num1 > num2:
-    print(num1)
-else :
-    print(num2)
+if A > B:
+    print(A)
+else:
+    print(B)
+    
+    
+[::-1]이 역순으로 바꾼다는 뜻이다
+[]이 있는걸 보면 슬라이싱인데 리스트가 아닌 문자열에도 쓸 수 있다는게 참 좋은것 같다
+reverse를 써봤는데 str이라고 오류가 났다
