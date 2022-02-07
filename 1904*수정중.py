62d4f96e9a85551b6fc4d729476366486ec322cd
purmpu <84269579+purmpu@users.noreply.github.com>
2022-02-07 14:35:39 +0900

    Create 1904*수정중.py

---
+++ 1904*수정중.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline())
+f = 1
+s = 2
+tmp = 0
+for i in range(n - 1):
+    tmp = f
+    f = s
+    s = (tmp + s) % 15746
+print(f)
+
+
+https://pacific-ocean.tistory.com/144
